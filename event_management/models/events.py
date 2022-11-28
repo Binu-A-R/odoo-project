4b626ef5389bd43fdd1dab7649442c99604aec57
binu <binuammankovi123@gmail.com>
2022-11-28 17:39:35 +0530

    [UPDT]completed the second task

--- event_management/models/events.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class EventManagement(models.Model):
-    _name = "event.property"
-    _description = "event.property"
-    name = fields.Char('Event Type:')
-    code = fields.Char('Code:')
-    image = fields.Binary('Image:')
